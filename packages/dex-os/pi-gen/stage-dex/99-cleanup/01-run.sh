5c822adc3604bbdd615f592dc4490c63a1e3a12e
Max Albrecht <1@178.is>
2024-04-22 20:39:22 +0200

    [WIP] dex-os-cleanup

--- packages/dex-os/pi-gen/stage-dex/99-cleanup/01-run.sh
+++ packages/dex-os/pi-gen/stage-dex/99-cleanup/01-run.sh
@@ -4,18 +4,21 @@ on_chroot << EOF
 
 systemctl disable bluetooth
 
-EOF
-
 apt purge -y build-essential python3-dev pi-bluetooth manpages manpages-dev
 apt autoremove -y --purge
 apt clean
 
-# we need to keep the following files for copyright compliance
-BAK_DIR="$(mktemp -d)"
-rsync -RavPh /usr/share/doc/*/copyright "${BAK_DIR}/"
+EOF
 
-rm -rf "${ROOTFS_DIR}/var/lib/apt/lists/"*
-rm -rf "${ROOTFS_DIR}/usr/share/"{locale,groff,doc,man,man-db}
+# we need to keep the following files for copyright compliance
+BAK_DIR="${ROOTFS_DIR}/tmp/doc-copyright"
+rsync -RavPh "${ROOTFS_DIR}"/usr/share/doc/*/copyright "${BAK_DIR}/"
+
+rm -rf "${ROOTFS_DIR}"/var/lib/apt/lists/*
+rm -rf "${ROOTFS_DIR}"/usr/share/{locale,groff,doc,man,man-db}
 
 # restore the copyright files
-rsync -avPh "${BAK_DIR}/usr/share/doc/" /usr/share/doc/
+rsync -avPh "${BAK_DIR}/usr/share/" "${ROOTFS_DIR}/usr/share/"
+rm -rf "${BAK_DIR}"
+
+
