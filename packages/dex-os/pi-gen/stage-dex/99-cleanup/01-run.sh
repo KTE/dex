7113041ebd757f3d36ec2af5aec1f47455259226
Max Albrecht <1@178.is>
2024-04-22 20:43:47 +0200

    [WIP] dex-os-cleanup

--- packages/dex-os/pi-gen/stage-dex/99-cleanup/01-run.sh
+++ packages/dex-os/pi-gen/stage-dex/99-cleanup/01-run.sh
@@ -10,15 +10,16 @@ apt clean
 
 EOF
 
-# we need to keep the following files for copyright compliance
-BAK_DIR="${ROOTFS_DIR}/tmp/doc-copyright"
-rsync -RavPh "${ROOTFS_DIR}"/usr/share/doc/*/copyright "${BAK_DIR}/"
+# # FIXME: more to patch in pi-gen/export-image/04-finalise/01-run.sh
+# # we need to keep the following files for copyright compliance
+# BAK_DIR="${ROOTFS_DIR}/tmp/doc-copyright"
+# rsync -RavPh "${ROOTFS_DIR}"/usr/share/doc/*/copyright "${BAK_DIR}/"
 
-rm -rf "${ROOTFS_DIR}"/var/lib/apt/lists/*
-rm -rf "${ROOTFS_DIR}"/usr/share/{locale,groff,doc,man,man-db}
+# rm -rf "${ROOTFS_DIR}"/var/lib/apt/lists/*
+# rm -rf "${ROOTFS_DIR}"/usr/share/{locale,groff,doc,man,man-db}
 
-# restore the copyright files
-rsync -avPh "${BAK_DIR}/usr/share/" "${ROOTFS_DIR}/usr/share/"
-rm -rf "${BAK_DIR}"
+# # restore the copyright files
+# rsync -avPh "${BAK_DIR}/usr/share/" "${ROOTFS_DIR}/usr/share/"
+# rm -rf "${BAK_DIR}"
 
 
